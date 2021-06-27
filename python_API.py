6dba8238fc2055849c998f2c2ef59bcda9bfa95b
pt21090 <31817347+pt21090@users.noreply.github.com>
2021-06-27 13:25:08 +0800

    Update python_API.py

--- python_API.py
+++ python_API.py
@@ -1,39 +1,36 @@
-from flask import Flask, request, abort
+import os
+from flask import Flask, abort, request
+# https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(os.environ.get("CCxxp2eejx8Ov1aZFA1l8ExjIu8p3JKQgAFZxXW4NsV+qDzYIWANU9HII9rNpjEXGBIz1+g1+3fwkfty9npA/SlUwmq70HWTh41NCb5x2Vb/9km2vOlRqUrTswn7oD2/xtiMsI/Tlooxfu3sSakNwAdB04t89/1O/w1cDnyilFU="))
+handler = WebhookHandler(os.environ.get("012eb4ff708b52457f34dd3dec8b40b7"))
 
-# LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
-handler = WebhookHandler('聊天機器人的 Channel secret')
 
-# 接收 LINE 的資訊
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=["GET", "POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
 
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
 
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
 
-    return 'OK'
+        return "OK"
 
-# 學你說話
+
 @handler.add(MessageEvent, message=TextMessage)
-def echo(event):
-    
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+def handle_message(event):
+    get_message = event.message.text
 
-if __name__ == "__main__":
-    app.run()
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
